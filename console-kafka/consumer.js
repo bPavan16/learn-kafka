8ace2be004ef33d4f5342d274690dcf62ed5f187
bPavan16 <pavanbhakta163@gmail.com>
2026-02-12 15:00:29 +0530

    added kafdrop to see kafka

--- console-kafka/consumer.js
+++ console-kafka/consumer.js
@@ -4,11 +4,14 @@ async function init() {
   const consumer = kafka.consumer({ groupId: "user-1" });
 
   console.log("Consumer Connecting...");
-  
+
+  // Connect to the Kafka cluster
   await consumer.connect();
 
+  // Subscribe to the topic "zeliot-topic" and read messages from the beginning of the topic
   await consumer.subscribe({ topic: "zeliot-topic", fromBeginning: true });
 
+  // Consume messages from the topic and log them to the console
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
       console.log({
@@ -20,7 +23,9 @@ async function init() {
     },
   });
 
+  // This is a comment this says nothing imp but this comment exists so that my code looks cool :D
   console.log("Consumer Connected!");
 }
 
+// run the consumer
 init();
